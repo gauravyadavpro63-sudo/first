ed1341e0d70c80bc9b4d53b277abff3c89bc225b
Gaurav Yadav <gyadav1905@gmail.com>
2026-06-21 19:02:58 +0530

    github

---
+++ github.js
@@ -0,0 +1,4 @@
+// git remote add origin https://github.com/gauravyadavpro63-sudo/first.git  {this will give your repa a name called origin}
+//git branch -M main {to change name of master to main}
+//git push -u origin main { when first time pushing your code to repo }
+//git push {to push code to repo}
