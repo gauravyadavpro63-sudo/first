289231a619b96e644b763b7a289fbff6cfbbf9ee
Gaurav Yadav <gyadav1905@gmail.com>
2026-06-21 19:40:56 +0530

    swah

--- github.js
+++ github.js
@@ -3,3 +3,11 @@
 //git push -u origin main { when first time pushing your code to repo }
 //git push {to push code to repo}
 //git push -u origin feature {when first time pushing branch to repo}
+//ham kabhi bhi main me chages nhi karte sidhe , pahle branch banate hai fir usko merge karte hai
+//company me branch main me merge jab hoga jab appka pull req accept hoga
+
+//bhut se developer push kar rahe hone so updated main ko apne system me pull karte hai;
+//git pull origin main
+//git branch -r { to check branches pushed on repo of github}
+//git branch { local machine me kitne brach hai }
+//git push origin --delete feature  {to delete branch pushed on repo}
